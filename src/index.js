4e20559171dc80619615330fb309c006be3e2c26
fitz-dev <tom.waters@myob.com>
2020-11-12 15:57:14 +1100

    finished tutorial (minus the storing of past gos) and have a working game.

--- src/index.js
+++ src/index.js
@@ -2,38 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
 
-//     render() {
-//         return (
-//             <button
-//                 className="square"
-//                 onClick={() => {
-//                     this.props.onClick()
-//                 }}>
-//                 {this.props.value}
-//             </button>
-//         );
-//     }
-// }
-
-// const Square = (props) => {
-//     let a = 'hello';
-//     return (
-//         <button className="square" onClick={props.onClick}>
-//             {props.value}
-//         </button>
-//     );
-// }
-
-const Square = (props) => (
-    <button className="square" onClick={props.onClick}>
-        {props.value}
-    </button>
-);
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
 
 class Board extends React.Component {
-    // TODO: https://reactjs.org/tutorial/tutorial.html#taking-turns
     constructor(props) {
         super(props);
         this.state = {
@@ -41,24 +19,37 @@ class Board extends React.Component {
             xIsNext: true,
         };
     }
+
     handleClick(i) {
         const squares = this.state.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({ 
+        this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
         });
     }
 
     renderSquare(i) {
-        return <Square
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
-        />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -100,8 +91,73 @@ class Game extends React.Component {
 }
 
 // ========================================
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+
+
+// class Square extends React.Component {
+
+//     render() {
+//         return (
+//             <button
+//                 className="square"
+//                 onClick={() => {
+//                     this.props.onClick()
+//                 }}>
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+// const Square = (props) => {
+//     let a = 'hello';
+//     return (
+//         <button className="square" onClick={props.onClick}>
+//             {props.value}
+//         </button>
+//     );
+// }
+
+//sols notes:
+// someFunction(initialValue){
+//     let value = initialValue;
+//     return [value, function(newValue){
+//         value = newValue;
+//     }];
+// }
+
+// const [value, updateValue] = someFunction(1);
+
+// updateValue(2);
+
+// const [squares, updateSquares] = useState(Array(9).fill(null));
+
+// handleClick = ({target}) => {
+//     const squares = [...squares];
+//     squares[target.value] = 'x';
+//     updateSquares(squares);
+// }
