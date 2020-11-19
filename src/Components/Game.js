19a072a8be4be84681db95a1f3d98d33ed84ec98
fitz-dev <tom.waters@myob.com>
2020-11-19 16:50:27 +1100

    completed tutorial

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -10,11 +10,12 @@ class Game extends React.Component {
                 squares: Array(9).fill(null),
             }],
             xIsNext: true,
+            stepNumber: 0,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1)
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -25,23 +26,31 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
+
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, moveNumber) => {
 
-            const desc = moveNumber ?
+            const buttonLabel = moveNumber ?
                 'Go to move #' + moveNumber :
                 'Go to game start';
             return (
-                <li>
-                    <button onClick={() => this.jumpTo(moveNumber)}>{desc}</button>
+                <li key={moveNumber}>
+                    <button onClick={() => this.jumpTo(moveNumber)}>{buttonLabel}</button>
                 </li>
             )
         })
@@ -68,7 +77,6 @@ class Game extends React.Component {
     }
 }
 
-// ========================================
 function calculateWinner(squares) {
     const lines = [
         [0, 1, 2],
